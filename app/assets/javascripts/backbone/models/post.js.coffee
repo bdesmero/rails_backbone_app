d7e6f9dd9bfd00732be674dd866568eb7a2e009d
bdesmero <bdesmero@gmail.com>
2014-06-06 13:20:12 +0800

    Push blog

---
+++ app/assets/javascripts/backbone/models/post.js.coffee
@@ -0,0 +1,6 @@
+class Blog.Models.Post extends Backbone.Model
+  paramRoot: 'post'
+
+class Blog.Collections.PostsCollection extends Backbone.Collection
+  model: Blog.Models.Post
+  url: '/posts'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '908d6c70be3c3485962aa67276ba61a7e8e6b5c551d0954e3ecc5334028c9b29060369016f75c57d9225697def49077fce2f1e0bd61ef930de68850a12a203b9'
